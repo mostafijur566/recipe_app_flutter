651fef7455c4a102cb5540730df6b659707d7b35
Mostafijur Rahman <66210185+mostafijur566@users.noreply.github.com>
2022-07-14 05:35:18 +0600

    home page recommended category added

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app_flutter/utils/app_colors.dart';
+import 'package:recipe_app_flutter/utils/dimensions.dart';
+
+import '../../widgets/big_text.dart';
+import '../../widgets/icon_and_text_widget.dart';
+import '../../widgets/small_text.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  PageController pageController = PageController(viewportFraction: 0.85);
+
+  var _currPageValue = 0.0;
+  double _scaleFactor = 0.8;
+  double _height = Dimensions.pageViewContainer;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bengali Recipe',
+          style: TextStyle(
+            color: AppColors.mainColor
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: Dimensions.pageView,
+            child: PageView.builder(
+              controller: pageController,
+                itemCount: 3,
+                itemBuilder: (context, position){
+                  return _buildPageItem(position);
+                }
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPageItem(int index) {
+    Matrix4 matrix = Matrix4.identity();
+
+    if (index == _currPageValue.floor()) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() - 1) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 0);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: Dimensions.pageViewContainer,
+            margin: EdgeInsets.only(
+                right: Dimensions.height10, left: Dimensions.height10, top: Dimensions.height10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(Dimensions.radius30),
+              color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  'assets/images/food_dummy.jpg'
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: Dimensions.pageContainerBox,
+              margin: EdgeInsets.only(
+                  right: Dimensions.height30,
+                  left: Dimensions.height30,
+                  bottom: Dimensions.height30),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(Dimensions.radius20),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Color(0xFFe8e8e8),
+                        blurRadius: 5.0,
+                        offset: Offset(0, 5)),
+                    BoxShadow(color: Colors.white, offset: Offset(-5, 0)),
+                    BoxShadow(color: Colors.white, offset: Offset(5, 0))
+                  ]),
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: Dimensions.height15,
+                    left: Dimensions.height15,
+                    right: Dimensions.height15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: 'Chicken wings', size: Dimensions.font26,),
+                    SizedBox(
+                      height: Dimensions.height10,
+                    ),
+                    Row(
+                      children: [
+                        Text('Recommended',
+                          style: TextStyle(
+                            color: AppColors.mainColor,
+                            fontWeight: FontWeight.bold
+                          )
+                        ),
+                        SizedBox(
+                          width: Dimensions.height30,
+                        ),
+                        IconAndTextWidget(
+                            icon: Icons.access_time_rounded,
+                            text: 'Cook Time 32min',
+                            iconColor: AppColors.iconColor2),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
