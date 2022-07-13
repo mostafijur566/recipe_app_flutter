4767414e9e0e4ec01630cb743fd5ac7e6b096190
Mostafijur Rahman <66210185+mostafijur566@users.noreply.github.com>
2022-07-14 05:34:36 +0600

    icon_and_text reusable widget added

---
+++ lib/widgets/icon_and_text_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:recipe_app_flutter/widgets/small_text.dart';
+
+import '../utils/dimensions.dart';
+
+class IconAndTextWidget extends StatelessWidget {
+  IconAndTextWidget({Key? key,
+    required this.icon,
+    required this.text,
+    required this.iconColor
+  }) : super(key: key);
+
+  final IconData icon;
+  final String text;
+  final Color iconColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(icon, color: iconColor, size: Dimensions.iconSize24,),
+        SizedBox(width: 5,),
+        SmallText(text: text,)
+      ],
+    );
+  }
+}
